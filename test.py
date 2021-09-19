91bb1b3d6da61331fc2e34027fbceffdf490b889
Miasoul <74416346+Miasoul@users.noreply.github.com>
2021-09-19 22:29:04 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,33 +1,46 @@
-import discord
+from collections import namedtuple
+import re
 from discord.ext import commands
+from discord.ext.commands import Bot, Context
+import discord
+from discord.ext.commands.converter import clean_content
+from discord.ext.commands.core import bot_has_guild_permissions
+import discord.utils
 import os
-import random
-from discord.ext.commands.core import check
-from discord_components import *
-from discordTogether import DiscordTogether
 client = commands.Bot(command_prefix=".")
-togec = DiscordTogether(client)
 
-@client.event
-async def on_ready():
-    print("ready")
-    DiscordComponents(client)
-    
+
 
 @client.command()
-async def 유튜브보기(ctx):
-    await ctx.send("음성채널에 들어간뒤 아래버튼을 클릭하고 ", components
-    = [Button(label = "CLICK me")])
-    interaction = await client.wait_for("button_click",
-    check = lambda i: i.component.label.startswith
-    ("CLICK"))
-    link = await togec.create_link(ctx.author.voice.channel.id, 'youtube')
-    await ctx.send(f"{link}")
+async def mute(ctx, member: discord.Member):
+     if ctx.message.author.guild_permissions.administrator:
+        
+        mutedRole = discord.utils.get(member.guild.roles, name='뮤트')
+        defRole = discord.utils.get(member.guild.roles, name ='Normal')
+        await member.add_roles(mutedRole)
+        await member.remove_roles(defRole)
+        
+        embed=discord.Embed(title="뮤트성공", description="**{0}** 이가  **{1}** 에게 뮤트 당했습니다!".format(member, ctx.message.author), color=0xff00f6)
+        await ctx.send(embed=embed)
+        
+     else:
+        embed=discord.Embed(title="권한거부", description="너 권한 없는뎅??", color=0xff00f6)
+        await ctx.send(embed=embed)
 
 @client.command()
-async def start(ctx):
-    link = await togec.create_link(ctx.author.voice.channel.id, 'youtube')
-    await ctx.send(f"{link}")
-
+async def unmute(ctx, member: discord.Member):
+     if ctx.message.author.guild_permissions.administrator:
+        
+        mutedRole = discord.utils.get(member.guild.roles, name='뮤트')
+        defRole = discord.utils.get(member.guild.roles, name ='Normal')
+        await member.add_roles(defRole)
+        await member.remove_roles(mutedRole)
+        
+        embed=discord.Embed(title="언뮤트성공", description="**{1}** 이가  **{0}** 을 언뮤트 했습니다!".format(member, ctx.message.author), color=0xff00f6)
+        await ctx.send(embed=embed)
+        
+     else:
+        embed=discord.Embed(title="권한거부", description="너 권한 없는뎅??", color=0xff00f6)
+        await ctx.send(embed=embed)
 access_token = os.environ['BOT_TOKEN']
 client.run(access_token)
