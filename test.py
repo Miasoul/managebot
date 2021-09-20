8249339ed53ea294ee6015cd108ceaa6cc8ff5b0
Miasoul <74416346+Miasoul@users.noreply.github.com>
2021-09-20 14:36:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,31 +8,9 @@ from discord.ext.commands.core import bot_has_guild_permissions
 import discord.utils
 import os
 
-client = commands.Bot(command_prefix=".")
+client = discord.Client()
 
-@client.command()
-async def randomteam(ctx):
-   print("뽑는중")
-   user = random.choice(ctx.message.channel.guild.members)
-   user2 = random.choice(ctx.message.channel.guild.members)
-   user3 = random.choice(ctx.message.channel.guild.members)
-   user4 = random.choice(ctx.message.channel.guild.members)
-   user5 = random.choice(ctx.message.channel.guild.members)
-   user6 = random.choice(ctx.message.channel.guild.members)
-   user7 = random.choice(ctx.message.channel.guild.members)
-   user8 = random.choice(ctx.message.channel.guild.members)
 
-   embedVar = discord.Embed(title="랜덤팀", description="랜덤팀", color=0x00ff00)
-   embedVar.add_field(name="주장", value=user, inline=False)
-   embedVar.add_field(name="팀원", value=user2, inline=False)
-   embedVar.add_field(name="팀원", value=user3, inline=False)
-   embedVar.add_field(name="팀원", value=user4, inline=False)
-   embedVar.add_field(name="팀2주장", value=user5, inline=False)
-   embedVar.add_field(name="팀2원", value=user6, inline=False)
-   embedVar.add_field(name="팀2원", value=user7, inline=False)
-   embedVar.add_field(name="팀2원", value=user8, inline=False)
-   
-   await ctx.channel.send(embed=embedVar)
 
 
 
